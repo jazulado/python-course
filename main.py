1cdf6b4fa988add0aa12079413e44e4d23481a51
206 <jazulado1337@hotmail.com>
2020-02-21 09:23:46 -0500

    update

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-edad1 = int(input("primer num "))
-edad2 = int(input("segundo num "))
+promedio = float(input("ingrese su promedio "))
+modalidad = "pregrado"
+creditos = 0
+descuento = 0
+podraMatricular = 1
 
-total = edad1 + edad2
-  
-print("Dos edades: " + str(total))
+if modalidad == "pregrado":
+    if promedio >= 4.5:
+        creditos = 28
+        descuento = 0.25
+    elif promedio < 4.5 and promedio >= 4:
+        creditos = 25
+        descuento = 0.1
+    elif promedio < 4 and promedio >= 3.5:
+        creditos = 20
+        descuento = 0
+    elif promedio < 3.5 and promedio >= 2.5:
+        print("Mayor: " + str(total))
+    elif promedio < 2.5:
+        print("no podra matricularse")
+        podraMatricular = 0
+else:
+    if promedio >= 4.5:
+        creditos = 20
+        descuento = 0.2
+    elif promedio < 4.5 and promedio >= 0:
+        creditos = 10
+        descuento = 0
+        
+if podraMatricular == 1:
+    if modalidad == "pregrado":
+        print("creditos: " + str(creditos) + " total a pagar: " + str(creditos* 50000))
+        print("descuento: " + str((creditos*50000)*descuento) + " total con descuento: " + str((creditos*50000)*(1-descuento)))
